8f01c931600f451235bbff428979f95231688d0d
ivanhoeNS <32396088+ivanhoeNS@users.noreply.github.com>
2019-08-05 11:36:58 +0200

    Add files via upload

---
+++ mainskript.js
@@ -0,0 +1,8 @@
+var dugme = document.getElementById('dugmedizajn');
+
+dugme.addEventListener ("click",redirekcija );
+
+function redirekcija() {
+
+	window.location ="http://www.ambijentdizajn.com" ;
+}
